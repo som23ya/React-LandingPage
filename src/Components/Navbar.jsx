d2a3bd1166e26ceb80679862eaec7edb029b673d
kritikashah20 <kritishah2017@gmail.com>
2021-05-26 17:07:36 +0530

    navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,20 +17,25 @@ function Navbar() {
 
     return (
         <nav className={nav ? 'nav active' : 'nav'}>
+            <span className='left-nav'>
             <a href='#main' className='logo'>
                 <img src={logo} alt=''/>
             </a>
+            <h3 className='h3-nav'>VISTAR</h3>
+            </span>
+            <span className='right-nav'>
             <input type='checkbox' className='menu-btn' id='menu-btn'/>
             <label className='menu-icon' for='menu-btn'>
                 <span className='nav-icon'></span>
             </label>
-            <span><h3>VISTAR</h3></span>
+            
             <ul className='menu'>
                 <li><a href='#main'>Home</a></li>
                 <li><a href='#features'>Features</a></li>
                 <li><a href='#about'>About</a></li>
                 <li><a href='https://vistar-jobs.netlify.app/'>Search/Post Job</a></li>
             </ul>
+            </span>
         </nav>
     )
 }
