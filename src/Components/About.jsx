72af3bbb7edc207f3f82451b56a0f9b478c524d2
Somya Upadhyay <43650322+som23ya@users.noreply.github.com>
2021-05-26 23:12:49 +0530

    updated about section

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,11 +8,11 @@ function About(props) {
             </div>
             <div className='about-text'>
                 <h2> {props.title} </h2>
-                <p>Vistaar is an online platform designed to provide a Centralized location for Employers to post about job Openings. The easy and user-friendly user interface of Vistaar makes it very easy for Job Seekers to look for their required Job of interest.
+                <p>Vistaar is an online platform designed to provide a centralized location for employers to display their job vacancies in our open post listings. The accessible and user-friendly interface of Vistaar also makes it very easy for Job Seekers to look for their required job of interest.
                     <br></br>
                     <br></br>
 
-It is designed especially to target the less educated section of the society by focussing on the Blue-Collar Jobs in their area. This platform allows Job Seekers to go through open listings of Jobs that are updated regularly and apply to the required Jobs by mentioning skills, education, experience, etc. A Job Seeker can Filter posted Jobs based on Site and type, Check the complete Description of Jobs, and apply on the go.</p>
+It is designed  to target especially the local community by focusing on the Blue-Collar Jobs in their area. This platform allows Job Seekers to go through open listings of jobs that are updated regularly and apply to the desired jobs by mentioning skills, education, experience, etc. A Job Seeker can filter posted jobs based on site and type, check the complete description of jobs, and apply on the go.</p>
             </div>
             
         </div>
