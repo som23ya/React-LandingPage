e431442c577a23c73319e0676ff0e5ff83da7a67
Somya Upadhyay <43650322+som23ya@users.noreply.github.com>
2021-05-27 01:21:18 +0530

    Update Feature.jsx

--- src/Components/Feature.jsx
+++ src/Components/Feature.jsx
@@ -8,9 +8,9 @@ function Feature() {
     return (
         <div id='features'>
             <h1><span>Features</span></h1>
-            <div className='a-container'> <FeatureBox image={featureimage} para='Open listing platform designed specifically for individuals from lower income households, made extremely easy-to-use with no login or authnetication needed.'/>
+            <div className='a-container'> <FeatureBox image={featureimage} para='Open listing platform designed specifically for individuals from low income households. Simple and quick application with no login or authentication needed from the user.'/>
                 <FeatureBox image={featureimage1} para='Job seekers can filter and seek jobs according to their preferred choices- either by site (on-site, remote, in office) or by type (full time, contract, part time, one time sevice).'/>
-                <FeatureBox image={featureimage2} para='A Live customer service chat focused on successful communication between businesses and their appliers is provided so anyone can ask for more details regarding the jobs.'/>
+                <FeatureBox image={featureimage2} para='A Live customer service chat focused on successful communication between businesses and their appliers is provided so anyone can ask for more details regarding the jobs or any other queries.'/>
           </div>
         </div>
     )
